6b54e382395f123ad97e17dc514aa67ee4dbd81f
Aaron Neustedter <aneusted@purdue.edu>
2020-08-08 22:00:12 -0400

    Now with response code and docker-compose file validation

--- services/container-maintainer.py
+++ services/container-maintainer.py
@@ -1,41 +1,56 @@
 #!/usr/bin/env python3
 
 import requests   # Used to get docker-compose from server
-import os.path    # Used to determine if there is already a local docker-compose file
+import os         # Used to check for existence of docker-compose files, move, and delete them
 import sys        # Used to exit the program
 import subprocess # Used to interact with docker-compose
 import socket     # Used to get hostname
 
 # Get current docker-compose from the server
 hostname = socket.gethostname()
-url = 'http://172.16.0.1:8006/' + hostname + '/docker-compose.yml'
-serverdc = requests.get(url).content.decode('ascii')
+url = 'http://172.16.0.1:8006/' + 'avena-apalis-dev06' + '/docker-compose.yml'
+print('Querying server for docker-compose file')
+response = requests.get(url)
 
-#TODO Ensure we don't have a 404, verify valid docker-compose
+# Ensure we got a valid response
+print('Ensuring we got a valid response')
+if response.status_code != 200:
+    print('Got a HTTP response code ', response, '. Is there a docker-compose.yml file in the right location?')
+    sys.exit()
+
+# Extract the docker-compose
+serverdc = response.content.decode('ascii')
+
+# Validate docker-compose
+open('./docker-compose.yml.new', 'w').write(serverdc)
+print('Validating new docker-compose')
+valid = subprocess.run(['docker-compose', '-f', './docker-compose.yml.new', 'config'])
+if valid.returncode != 0:
+    print('Return code is not valid! Not using new docker-compose')
+    os.remove('./docker-compose.yml.new')
+    sys.exit()
 
 if not os.path.isfile('./docker-compose.yml'):
-    # If no current docker-compose, write it and exit
+    # If no current docker-compose, write it
     print('Writing initial docker-compose from server')
-    open('./docker-compose.yml', 'w').write(serverdc)
-    print('Exiting')
-    sys.exit()
+    os.rename('./docker-compose.yml.new', './docker-compose.yml')
+else:
+    print('docker-compose file found, comparing to server version')
 
-print('docker-compose file found, comparing to server version')
+    # Get docker-compose config
+    localdc = ''
+    with open ("./docker-compose.yml", "r") as openfile:
+        localdc = openfile.read()
 
-# Get docker-compose config
-localdc = ''
-with open ("./docker-compose.yml", "r") as openfile:
-    localdc = openfile.read()
+    # If they are the same, exit
+    if localdc == serverdc:
+        print('Server and local docker-compose files are the same. Exiting')
+        sys.exit()
 
-# If they are the same, exit
-if localdc == serverdc:
-    print('Server and local docker-compose files are the same. Exiting')
-    sys.exit()
-
-# Write the new docker-compose
-print('Server and local docker-compose files are not the same. Replacing docker-compose and restarting all docker containers')
-open('./docker-compose.yml', 'w').write(serverdc)
-print('New docker-compose written. Stopping and removing all docker containers')
+    # Write the new docker-compose
+    print('Server and local docker-compose files are not the same. Replacing docker-compose and restarting all docker containers')
+    os.rename('./docker-compose.yml.new', './docker-compose.yml')
+    print('New docker-compose written. Stopping and removing all docker containers')
 
 # Get a list of the currently running docker containers
 print('Getting current containers')
