96e1901c513bba5cc4990801d34f8fd587e99ce9
Aaron Neustedter <aneusted@purdue.edu>
2020-08-28 18:02:49 -0400

    Bugfixes and verification testing on dev board 5

--- services/can-watchdog/can-watchdog.py
+++ services/can-watchdog/can-watchdog.py
@@ -1,26 +1,39 @@
 #!/usr/bin/python3
-import glob # For expanding wildcard filepaths
-import jeepney # For interactions with dbus
-from subprocess import call
 
-print('CAN Watchtog has started')
-print('Getting all can rx counters')
-rx_paths = glob.glob('sys/class/net/can*/statistics/rx_bytes')
+import glob # For expanding wildcard filepaths
+from subprocess import call
+from time import sleep
+import sys
+
+print('CAN Watchdog has started')
+print('Getting all can rx_bytes counters')
+rx_paths = glob.glob('/sys/class/net/can*/statistics/rx_bytes')
 print(len(rx_paths), ' found: ', rx_paths)
 
 prev_rx = 0
 curr_rx = 0
 
-sleep(5)
+rx_fds = []
+for rx in rx_paths:
+    rx_fds.append(open(rx, 'r'))
 
 while True:
-    for rx in rx_paths
-        with open(rx, 'r') as file:
-            curr_rx += int(file.read())
-            
+    print('Sleeping 5')
+    sleep(5)
+    for fd in rx_fds:
+        fd.seek(0, 0)
+        rx_str = fd.read().strip()
+        if rx_str.isdigit():
+            curr_rx += int(rx_str)
+        else:
+            print('Could not coerce `', rx_str, '` to an integer, skipping') 
+ 
+    print('rx bytes: ', curr_rx)       
     if curr_rx == prev_rx:
-        print('Requesting sleep')
+        print('Requesting suspend')
         call(["systemctl", "suspend"])
+        sleep(5)
     else:
         prev_rx = curr_rx
+        curr_rx = 0
 
