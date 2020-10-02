52c0e66be83dd1f8e1a9fe9705187d479d1a68ac
Fabio A. Castiblanco <fcastibl@purdue.edu>
2020-10-01 23:28:46 -0400

    Added CAN interface selection env. variables

--- services/socketcand/socketcand.sh
+++ services/socketcand/socketcand.sh
@@ -2,5 +2,6 @@
 
 port=${port:-20001}
 interface=${interface:-docker0}
-uname -a
-/usr/src/app/socketcand -v -p $port -l $interface
+can_interfaces=${can_interfaces:can0,can1}
+
+/usr/src/app/socketcand -v -p $port -l $interface -i $can_interfaces
