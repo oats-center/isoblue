3ff2022308ba85aa1e88e9951cb70b0020dbb6a9
Jarett Lee <jarett.lee.pi@gmail.com>
2020-10-05 02:06:40 +0000

    Add logging and grab log level from env

--- services/gps2tsdb/gps.py
+++ services/gps2tsdb/gps.py
@@ -5,18 +5,26 @@ import dbus
 import postgres
 import os
 import sys
+import logging
 from prometheus_client import start_http_server, Gauge
 
+# Accepts strings and integers
+env_log_level = os.environ.get('LOGLEVEL', 'WARNING').upper()
+log_level = int(env_log_level) if env_log_level.isdigit() else env_log_level
+logging.basicConfig(
+    level=log_level,
+    format='%(asctime)s.%(msecs)03d UTC %(levelname)-8s %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S')
+
 def fix(*args):
-    #print(args)
-    print("Time: ", args[0], "\tLat: ", args[3], "\tLng: ", args[4])
+    logging.info("Time: ", args[0], "\tLat: ", args[3], "\tLng: ", args[4])
     lat_gauge.set(args[3])
     lng_gauge.set(args[4])
     time_gauge.set(args[0])
 
-    print("Inserting lat and lng for timestanp ", args[0])
+    logging.info("Inserting lat and lng for timestanp ", args[0])
     db.run("INSERT INTO gps (time, lat, lng) VALUES (to_timestamp(%s), %s, %s)", (float(args[0]), float(args[3]), float(args[4]) ) )
-    print("Finsihed inserting lat and lng for timestamp ", args[0])
+    logging.info("Finsihed inserting lat and lng for timestamp ", args[0])
 
     sys.stdout.flush()
 
@@ -30,20 +38,20 @@ start_http_server(10001)
 
 global db
 connectionurl='postgresql://' + os.environ['db_user'] + ':' + os.environ['db_password'] + '@postgres:' + os.environ['db_port'] + '/' + os.environ['db_database']
-print("Initing Postgres obj")
+logging.info("Initing Postgres obj")
 db = postgres.Postgres(url=connectionurl)
 
-print("Ensuring timescaledb extension is activated")
+logging.info("Ensuring timescaledb extension is activated")
 db.run("CREATE EXTENSION IF NOT EXISTS timescaledb;")
-print("Ensuring tables are setup properly")
+logging.info("Ensuring tables are setup properly")
 db.run("""
         CREATE TABLE IF NOT EXISTS gps (
           time timestamptz UNIQUE NOT NULL,
           lat double precision NOT NULL,
           lng double precision NOT NULL);""")
-print("Ensuring GPS point table is a timescaledb hypertable")
+logging.info("Ensuring GPS point table is a timescaledb hypertable")
 db.run("SELECT create_hypertable('gps', 'time', if_not_exists => TRUE, migrate_data => TRUE);")
-print("Finished settting up tables")
+logging.info("Finished settting up tables")
 
 
 DBusGMainLoop(set_as_default=True)
