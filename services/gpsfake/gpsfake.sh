113e4f4a63cfae75dd18e91bbe941cbf14de9e18
Aaron Neustedter <aneusted@purdue.edu>
2020-10-28 22:40:42 -0400

    Additional debugging

--- services/gpsfake/gpsfake.sh
+++ services/gpsfake/gpsfake.sh
@@ -12,10 +12,16 @@
 #echo "Starting dbus daemon"
 #mkdir -p /var/run/dbus
 #dbus-daemon --config-file=/usr/share/dbus-1/system.conf --print-address
+#cat /usr/share/dbus-1/system.conf
 
 #Sleep for 10s to allow gps2tsdb and the database to start up
+echo "Sleeping 10 to allow startup"
 sleep 10
+
+echo "Starting dbus-monitor in the background"
+#dbus-monitor --system "interface='org.gpsd'" &
+#tcpdump -s0 -n -A -i lo udp and port 5000 &
 
 # Run GPS fake
 echo "Starting gpsfake"
-gpsfake -l -S fake-gps.log
+gpsfake -1 -l -S -n -D 2 fake-gps.log
