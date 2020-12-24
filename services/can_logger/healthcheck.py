1b90629a2a7f04c0f7e542ae68c886204f745bef
Aaron Neustedter <aneusted@purdue.edu>
2020-12-24 16:52:24 -0500

    Bus activity check
    Added functionality to healthcheck to check if there is bus activity and use that to determine health.
    If there is no activity on the bus, then the logger is not expected to be doing anything and is still
    presumed to be healthy.
    
    Also added some sysout debug flushes to can_logger that assisted me in debugging

--- services/can_logger/healthcheck.py
+++ services/can_logger/healthcheck.py
@@ -3,6 +3,7 @@ import postgres
 import sys
 import os
 from datetime import datetime
+from time import sleep
 
 # Take name of service, timestamp of last update, and threshold
 # Compares the current time to the passed timestamp, and exits or outputs the appropriate message
@@ -25,35 +26,74 @@ threshold = 5
 # Get environmental variable determining logging to csv or db or both
 default_value = 'CSV'
 log_env = os.getenv('CELL_LOG', default_value)
-host_interface = os.environ['can_interface']
+host_interfaces = os.environ['can_interface'].split(',')
 
-# If the container is logging to csv
-if 'CSV' in log_env.upper():
-    logpath = '/data/log/' + host_interface + '.csv'
-    # Ensure the file exists in the first place
-    if not os.path.exists(logpath):
-         print('Log file did not exist or was not able to be opened')
-         sys.exit(1)
+for interface in host_interfaces:
 
-    # Get the last time the file was modified
-    lastupdate = datetime.fromtimestamp(os.path.getmtime(logpath))
-    checktimestamp(logpath, lastupdate, threshold)
+    # Ensure that there is actually being data logged to the bus
+    # Currently there are two hacky ways to do this. Either we can
+    # check the amount of rx_bytes, write them to a file, and then
+    # check for changes next time we run this script, or we can read
+    # the file, sleep for a certain period of time, and then check again
+    # Currently we are trying the later as it is a bit simpler although
+    # slower to respond due to the sleep
+    rx_start = -1
+    rx_end = -1
+    with open('/mnt/host/sys/class/net/' + interface + '/statistics/rx_bytes', 'r') as rx:
+        rx_start = int(rx.read().strip())
+        sleep(1)
+        rx.seek(0,0)
+        rx_end = int(rx.read().strip())
+    
+    print(interface, 'rx_end:', rx_end, 'rx_start:', rx_start)
+    if (rx_end - rx_start) <= 0:
+        print('No can messages sent on the bus, no expected action by can logger')
+        continue
+    '''
+    rxbfile = interface + '_rx_bytes.tmp'
+    if os.path.exists(rxbfile):
+         with open(rxbfile, 'rw') as rxb_write, open('/mnt/host/sys/class/net/' + interface + '/statistics/rx_bytes', 'r') as rxb_read:
+             old_read = int(rxb_write.read().strip())
+             new_read = int(rxb_read.read().strip())
+             old_read.write(str(new_read))
+             if (new_read - old_read) <= 0:
+                 print('No can messages sent on', interface, 'no expected action by logger')
+                 continue
 
-# If the container is logging to database
-if 'DB' in log_env.upper():
-    # Connect to the database
-    connectionurl='postgresql://' + os.environ['db_user'] + ':' + os.environ['db_password'] + '@postgres:' + os.environ['db_port'] + '/' + os.environ['db_database']
-    db = postgres.Postgres(url=connectionurl)
 
-    # Query the most recent timestamp
-    rst = db.one("SELECT * FROM can ORDER BY time DESC LIMIT 1;")
+    else:
+        with open(rxbfile, 'w') as rxb_write, open('/mnt/host/sys/class/net/' + interface + '/statistics/rx_bytes', 'r') as rxb_read:
+            rxb_write(rxb_read().strip())
+            print('Created temp file for next run with interface', interface)
+            continue
+    '''
+    # If the container is logging to csv
+    if 'CSV' in log_env.upper():
+        logpath = '/data/log/' + interface + '.csv'
+        # Ensure the file exists in the first place
+        if not os.path.exists(logpath):
+             print('Log file did not exist or was not able to be opened. Path: ',logpath)
+             sys.exit(1)
 
-    # Handle if the database is empty
-    if rst == None:
-        print("Database has the wrong schema or no data");
-        sys.exit(1)
+        # Get the last time the file was modified
+        lastupdate = datetime.fromtimestamp(os.path.getmtime(logpath))
+        checktimestamp(logpath, lastupdate, threshold)
 
-    # First column is the timestamp, in datetime format
-    lastupdate = rst[0]
-    checktimestamp('CAN signal database table', lastupdate, threshold)
+    # If the container is logging to database
+    if 'DB' in log_env.upper():
+        # Connect to the database
+        connectionurl='postgresql://' + os.environ['db_user'] + ':' + os.environ['db_password'] + '@postgres:' + os.environ['db_port'] + '/' + os.environ['db_database']
+        db = postgres.Postgres(url=connectionurl)
+
+        # Query the most recent timestamp
+        rst = db.one("SELECT * FROM can ORDER BY time where can_interface = ? DESC LIMIT 1;", interface)
+
+        # Handle if the database is empty
+        if rst == None:
+            print("Database has the wrong schema or no data");
+            sys.exit(1)
+
+        # First column is the timestamp, in datetime format
+        lastupdate = rst[0]
+        checktimestamp(interface + ' CAN signal database table', lastupdate, threshold)
 
