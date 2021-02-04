bb0faa3eaa2b6132894cd421c3f9ba9389dc1acb
Aaron Neustedter <aneusted@purdue.edu>
2021-02-04 16:28:18 -0500

    Migrate to NATS

--- services/gps2tsdb/test/gps_replay/gps_replay.py
+++ services/gps2tsdb/test/gps_replay/gps_replay.py
@@ -6,203 +6,205 @@ import os              # Used for reading envrion vars
 import json            # For reading test_points.log file
 import datetime        # For converting posix timestamp to epoc
 import math            # For converting x and y error to total hoiz error
-import zmq             # For publishing gps points for verification containers
+import asyncio         
+from nats.aio.client import Client as NATS
+from nats.aio.errors import ErrConnectionClosed, ErrTimeout, ErrNoServers
 
 # For interacting with dbus to send messages
 from jeepney import DBusAddress, new_signal
 from jeepney.integrate.blocking import connect_and_authenticate
 
-# Location of log file to insert into database
-testpointsloc = '/opt/test_points.log'
+async def run(loop):
+    # Location of log file to insert into database
+    testpointsloc = '/opt/test_points.log'
+    
+    # Equivalent to 
+    #
+    # sudo dbus-send --system --type=signal /org/gpsd org.gpsd.fix \
+    # double:{Seconds since unix epoc} \
+    # int32:{mode} \     
+    # double:{time uncertainty (seconds)} \  
+    # double:{Lat in deg} \ 
+    # double:{Lng in deg} \
+    # double:{Horiz uncertainty} \ 
+    # double:{Altitude} \
+    # double:{Altitude uncertainty} \ 
+    # double:{Course in deg from true north} \
+    # double:{Course uncertainty} \    
+    # double:{Speed} \
+    # double:{Speed uncertainty} \     
+    # double:{Climb} \
+    # double:{Climb uncertainty} \   
+    # string:{Device name}
+    
+    # Based off this blocking example from jeepney docs found here: 
+    #   https://jeepney.readthedocs.io/en/latest/integrate.html
+    def gps_to_dbus(dbusargs):
+        if len(dbusargs) != 15:
+            print('tuple argument for gps_to_dbus was not valid. Expected 15 entries, got',len(dbusargs))
+            return -1
+    
+        gps = DBusAddress('/org/gpsd',
+                           bus_name=None,
+                           interface='org.gpsd')
+    
+        connection = connect_and_authenticate(bus='SYSTEM')
+    
+        # Construct a new D-Bus message. new_method_call takes the address, the
+        # method name, the signature string, and a tuple of arguments.
+        # 'signature string' is not documented well, it's basically the argument types. More info here
+        #    https://dbus.freedesktop.org/doc/dbus-specification.html#type-system
+        msg = new_signal(gps, 'fix', 'didddddddddddds', dbusargs )
+    
+        # Send the message and wait for the reply
+        reply = connection.send_message(msg)
+        #print('Reply: ', reply)
+    
+        connection.close()
+    
+    if not os.path.isfile(testpointsloc):
+        print('Test points file does not exist in', testpointsloc, ', cannot insert test points')
+        exit(-1)
+    
+    print('Setting up NATS')
+    
+    # Create nats object
+    nc = NATS()
+    
+    # Connect to local nats server
+    await nc.connect("nats://nats:4222")
+    
+    # Send sync request. When we get a reply, we can assume that the verifcation container is ready for
+    # us to start sending messages
+    # Losely based off this example: https://docs.nats.io/developing-with-nats/sending/request_reply
+    print('Waiting 10s for gps2tsdb and postgres to startup')
+    sleep(10)
 
-# Equivalent to 
-#
-# sudo dbus-send --system --type=signal /org/gpsd org.gpsd.fix \
-# double:{Seconds since unix epoc} \
-# int32:{mode} \     
-# double:{time uncertainty (seconds)} \  
-# double:{Lat in deg} \ 
-# double:{Lng in deg} \
-# double:{Horiz uncertainty} \ 
-# double:{Altitude} \
-# double:{Altitude uncertainty} \ 
-# double:{Course in deg from true north} \
-# double:{Course uncertainty} \    
-# double:{Speed} \
-# double:{Speed uncertainty} \     
-# double:{Climb} \
-# double:{Climb uncertainty} \   
-# string:{Device name}
+    with open(testpointsloc) as f:
+        for line in f:
+            point = ''
+            try:
+                 point = json.loads(line)
+            except ValueError as e:
+                 continue
+            # Read the line in as JSON and convert it to python object
+            point = json.loads(line)
+            # Skip points that do not have a full 3D fix 
+            if point['class'] == 'TPV' and point['mode'] == 3:
+    
+                # Convert ISO time string into posix epoch time
+                timeraw = point['time']
+                if timeraw[-1] == 'Z':
+                  timeraw = timeraw[:-1]
+                time_epoch = datetime.datetime.fromisoformat(timeraw).timestamp()
+    
+    
+                # So this gets really nasty. Basically we have a list of key-value pairs and we need to put the values into an ordered tuple.
+                # We are not guaranteed the order or even existence of any given key (I am assuming that time, mode, and device will always exist as the 
+                # points are pretty useless without time plus mode and device _should_ always be known as they are independent of the device having a gps fix). 
+                # If the key does not exist 'NaN' should be inserted instead
+                # I am creating a list that I will insert each item from the json tuple into and then convert into a tuple. I check for the existence of each
+                # point and insert nan or the point. There may be a better way. I considered a tuple of all the keys and then iterating through them but some
+                # points require special attention (ex altitude). Mappings were found from here: https://gitlab.com/gpsd/gpsd/-/blob/release-3.20/dbusexport.c#L55
+                #  At some point the data log should be modified to add extreme values as well
+    
+                dbuslist = []
+                dbuslist.append(time_epoch)
+    
+                dbuslist.append(point['mode'])
+                if 'ept' in point:
+                    dbuslist.append(point['ept'])
+                else:
+                    dbuslist.append(float('nan'))
+    
+                if 'lat' in point:
+                    dbuslist.append(point['lat'])
+                else:
+                    dbuslist.append(float('nan'))
+    
+                if 'lon' in point:
+                    dbuslist.append(point['lon'])
+                else:
+                    dbuslist.append(float('nan'))
+    
+                if 'eph' in point:
+                    dbuslist.append(point['eph'])
+                else: 
+                    dbuslist.append(float('nan'))
+    
+                if 'alt' in point:
+                    dbuslist.append(point['alt'])
+                elif 'altMSL' in point:
+                    dbuslist.append(point['altMSL'])
+                else:
+                    dbuslist.append(float('nan'))
+    
+                if 'epv' in point:
+                    dbuslist.append(point['epv'])
+                else:
+                    dbuslist.append(float('nan'))
+    
+                if 'track' in point:
+                    dbuslist.append(point['track'])
+                else:
+                    dbuslist.append(float('nan'))
+    
+                if 'epd' in point:
+                    dbuslist.append(point['epd'])
+                else:
+                    dbuslist.append(float('nan'))
+    
+                if 'speed' in point:
+                    dbuslist.append(point['speed'])
+                else:
+                    dbuslist.append(float('nan'))
+    
+                if 'eps' in point:
+                    dbuslist.append(point['eps'])
+                else:
+                    dbuslist.append(float('nan'))
+    	
+                if 'climb' in point:
+                    dbuslist.append(point['climb'])
+                else:
+                    dbuslist.append(float('nan'))
+    
+                if 'epc' in point:
+                    dbuslist.append(point['epc'])
+                else:
+                    dbuslist.append(float('nan'))
+    
+                # Using a constant to ensure this is never confused for real data
+                dbuslist.append('fake-data-verification')
+    
+                # convert to tuple to send to dbus method
+                tup = tuple(dbuslist)
+    		    
+                print('Tuple:', tup)
+                gps_to_dbus(tup)
+            
+                # Don't overpower gps2tsdb
+                # Basically if this is not here, gps_verify will check for the point before
+                # gps2tsdb has the chance to enter it into the db
+                sleep(.1)
+    
+                # Publish GPS point to zeromq
+                print('Publishing gps data')
+                await nc.publish("gps", json.dumps(point).encode());
+                await nc.flush(1)
+                sys.stdout.flush()
+    
+    print('Sending end messages')
+    await nc.publish("gps", json.dumps("END").encode())
+    await nc.close()
 
-# Based off this blocking example from jeepney docs found here: 
-#   https://jeepney.readthedocs.io/en/latest/integrate.html
-def gps_to_dbus(dbusargs):
-    if len(dbusargs) != 15:
-        print('tuple argument for gps_to_dbus was not valid. Expected 15 entries, got',len(dbusargs))
-        return -1
 
-    gps = DBusAddress('/org/gpsd',
-                       bus_name=None,
-                       interface='org.gpsd')
-
-    connection = connect_and_authenticate(bus='SYSTEM')
-
-    # Construct a new D-Bus message. new_method_call takes the address, the
-    # method name, the signature string, and a tuple of arguments.
-    # 'signature string' is not documented well, it's basically the argument types. More info here
-    #    https://dbus.freedesktop.org/doc/dbus-specification.html#type-system
-    msg = new_signal(gps, 'fix', 'didddddddddddds', dbusargs )
-
-    # Send the message and wait for the reply
-    reply = connection.send_message(msg)
-    #print('Reply: ', reply)
-
-    connection.close()
-
-if not os.path.isfile(testpointsloc):
-    print('Test points file does not exist in', testpointsloc, ', cannot insert test points')
-    exit(-1)
-
-print('Setting up zeromq')
-# Modified example from https://zguide.zeromq.org/docs/chapter2/#Node-Coordination
-subs_expected = 1
-zmq_ctx = zmq.Context()
-
-# Socket to talk to clients
-zmq_pub = zmq_ctx.socket(zmq.PUB)
-# set SNDHWM, so we don't drop messages for slow subscribers
-zmq_pub.sndhwm = 1100000
-zmq_pub.bind('tcp://*:5561')
-
-# Socket to receive signals
-zmq_sync = zmq_ctx.socket(zmq.REP)
-zmq_sync.bind('tcp://*:5562')
-
-# Get synchronization from subscribers
-print('awaiting', subs_expected, 'subs')
-subscribers = 0
-while subscribers < subs_expected:
-    # wait for synchronization request
-    msg = zmq_sync.recv()
-    # send synchronization reply
-    zmq_sync.send(b'')
-    subscribers += 1
-    print('+1 subscriber (%i/%i)' % (subscribers, subs_expected))
-
-with open(testpointsloc) as f:
-    for line in f:
-        point = ''
-        try:
-             point = json.loads(line)
-        except ValueError as e:
-             continue
-        # Read the line in as JSON and convert it to python object
-        point = json.loads(line)
-        # Skip points that do not have a full 3D fix 
-        if point['class'] == 'TPV' and point['mode'] == 3:
-
-            # Convert ISO time string into posix epoch time
-            timeraw = point['time']
-            if timeraw[-1] == 'Z':
-              timeraw = timeraw[:-1]
-            time_epoch = datetime.datetime.fromisoformat(timeraw).timestamp()
+if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(run(loop))
+    loop.close()
+    print('All points successfully inserted into database. Staying alive so that exit code for gps_verify is used')
+    while(True):
+        sleep(10000)
 
 
-            # So this gets really nasty. Basically we have a list of key-value pairs and we need to put the values into an ordered tuple.
-            # We are not guaranteed the order or even existence of any given key (I am assuming that time, mode, and device will always exist as the 
-            # points are pretty useless without time plus mode and device _should_ always be known as they are independent of the device having a gps fix). 
-            # If the key does not exist 'NaN' should be inserted instead
-            # I am creating a list that I will insert each item from the json tuple into and then convert into a tuple. I check for the existence of each
-            # point and insert nan or the point. There may be a better way. I considered a tuple of all the keys and then iterating through them but some
-            # points require special attention (ex altitude). Mappings were found from here: https://gitlab.com/gpsd/gpsd/-/blob/release-3.20/dbusexport.c#L55
-            #  At some point the data log should be modified to add extreme values as well
-
-            dbuslist = []
-            dbuslist.append(time_epoch)
-
-            dbuslist.append(point['mode'])
-            if 'ept' in point:
-                dbuslist.append(point['ept'])
-            else:
-                dbuslist.append(float('nan'))
-
-            if 'lat' in point:
-                dbuslist.append(point['lat'])
-            else:
-                dbuslist.append(float('nan'))
-
-            if 'lon' in point:
-                dbuslist.append(point['lon'])
-            else:
-                dbuslist.append(float('nan'))
-
-            if 'eph' in point:
-                dbuslist.append(point['eph'])
-            else: 
-                dbuslist.append(float('nan'))
-
-            if 'alt' in point:
-                dbuslist.append(point['alt'])
-            elif 'altMSL' in point:
-                dbuslist.append(point['altMSL'])
-            else:
-                dbuslist.append(float('nan'))
-
-            if 'epv' in point:
-                dbuslist.append(point['epv'])
-            else:
-                dbuslist.append(float('nan'))
-
-            if 'track' in point:
-                dbuslist.append(point['track'])
-            else:
-                dbuslist.append(float('nan'))
-
-            if 'epd' in point:
-                dbuslist.append(point['epd'])
-            else:
-                dbuslist.append(float('nan'))
-
-            if 'speed' in point:
-                dbuslist.append(point['speed'])
-            else:
-                dbuslist.append(float('nan'))
-
-            if 'eps' in point:
-                dbuslist.append(point['eps'])
-            else:
-                dbuslist.append(float('nan'))
-	
-            if 'climb' in point:
-                dbuslist.append(point['climb'])
-            else:
-                dbuslist.append(float('nan'))
-
-            if 'epc' in point:
-                dbuslist.append(point['epc'])
-            else:
-                dbuslist.append(float('nan'))
-
-            # Using a constant to ensure this is never confused for real data
-            dbuslist.append('fake-data-verification')
-
-            # convert to tuple to send to dbus method
-            tup = tuple(dbuslist)
-		    
-            print('Tuple:', tup)
-            gps_to_dbus(tup)
-        
-            # Don't overpower gps2tsdb
-            sleep(.1)
-
-            # Publish GPS point to zeromq
-            print('Publishing gps data')
-            zmq_pub.send_json(point)
-            sys.stdout.flush()
-
-print('Sending end messages')
-zmq_pub.send_json(json.dumps("END"))
-
-print('All points successfully inserted into database. Staying alive so that exit code for gps_verify is used')
-while(True):
-    sleep(10000)
--- services/gps2tsdb/test/gps_verify/gps_verify.py
+++ services/gps2tsdb/test/gps_verify/gps_verify.py
@@ -1,75 +1,104 @@
 #!/usr/bin/python3
-import zmq
 import postgres
+from psycopg2 import OperationalError
 import time
 import os
 import sys
 import json
+import asyncio         
+from nats.aio.client import Client as NATS
+from nats.aio.errors import ErrConnectionClosed, ErrTimeout, ErrNoServers
 
-print('Waiting 10s for gps2tsdb and postgres to startup')
-time.sleep(10)
-
-print('Connecting to the database')
-connectionurl='postgresql://' + os.environ['db_user'] + ':' + os.environ['db_password'] + '@postgres:' + os.environ['db_port'] + '/' + os.environ['db_database']
-print('Initing Postgres obj')
-db = postgres.Postgres(url=connectionurl)
-
-# This should be replaced query that deletes all lines from the device 'fake gps' or whatever but until
-# gps device is recorded this is what we got 
-print('Truncating GPS database')
-db.run('TRUNCATE gps;')
-
-# Based on example from here: https://zguide.zeromq.org/docs/chapter2/#Node-Coordination
-print('Setting up zeromq')
-zmq_ctx = zmq.Context()
-
-# First, connect our subscriber socket
-print('Connecting to zmq sub socket')
-zmq_sub = zmq_ctx.socket(zmq.SUB)
-zmq_sub.connect('tcp://gps_replay:5561')
-zmq_sub.setsockopt(zmq.SUBSCRIBE, b'')
-
-time.sleep(1)
-
-# Second, synchronize with publisher
-print('Connecting to zmq sync socket')
-zmq_sync = zmq_ctx.socket(zmq.REQ)
-zmq_sync.connect('tcp://gps_replay:5562')
-
-# send a synchronization request
-print('Sending sync zmq request')
-zmq_sync.send(b'')
-
-# wait for synchronization reply
-print('Awaiting zmq sync reply')
-zmq_sync.recv()
-
-# Third, get our updates and report how many we got
 points_received = 0
-while True:
-  
-  print('Awaiting zmq gps point')
-  zmq_gps_point = zmq_sub.recv_json()
 
-  # Check if this is a control message signifying the end of the transmission
-  if zmq_gps_point == json.dumps("END"): 
-    print('End control message received. No more gps points to check')
-    break
-
-  # Check for zmq's point
-  print('Verifying that zmq', zmq_gps_point['time'], ' is in the database')
-  rst = db.one('SELECT * FROM gps where time = %s;', (zmq_gps_point['time'],) )
-  if rst is not None and len(rst) == 3 and rst.lat == zmq_gps_point['lat'] and rst.lng == zmq_gps_point['lon']:
-    print('zmq timestamp', zmq_gps_point['time'], 'successfully entered into the database')
-    points_received
-  else:
-    print('zmq GPS point', zmq_gps_point, '\nwas not successfully entered into database!\ndb entry:', rst)
-    sys.exit(-1)
+async def run(loop):
   
-  # Counter just for fun
-  points_received = points_received + 1
+  print('Connecting to the database')
+  connectionurl='postgresql://' + os.environ['db_user'] + ':' + os.environ['db_password'] + '@postgres:' + os.environ['db_port'] + '/' + os.environ['db_database']
+
+  # Try to connect to the DB. It may be starting up so we should try a few timees
+  # before failing. Currently trying every second 60 times
+  tries = 0
+  maxtries = 60
+  sleeptime = 1
+  db_connected = False
+  while not db_connected:
+    try:
+      db = postgres.Postgres(url=connectionurl)
+    except OperationalError as e:
+      if tries < maxtries:
+        print("Database connection attempt", tries, "failed. Database may still be starting. Sleeping", sleeptime, "s and trying again")
+        print(e)
+        tries = tries + 1
+        time.sleep(sleeptime)
+      else:
+        print("FATAL: Could not connect to db after", tries, "tries. Exiting")
+        sys.exit(-1)
+    else:
+      print("DB successfully connected")
+      db_connected = True
+
+  # Sleep for 2 seconds to allows gps2tsdb to create the gps table
+  time.sleep(5)
+
+  # This should be replaced query that deletes all lines from the device 'fake gps' or whatever but until
+  # gps device is recorded this is what we got 
+  print('Truncating GPS database')
+  db.run('TRUNCATE gps;')
+    
+  print('Setting up NATS')
+  # Connect to NATS server 
+  nc = NATS()
+  await nc.connect("nats://nats:4222")
+  
+  # Notifies the non-callback function that we are finished
+  fin = asyncio.Future() 
+
+  async def message_handler(msg): 
+    nats_gps_point = json.loads(msg.data.decode())
+
+    # Debugging
+    #print("NATS type:", type(nats_gps_point), "\nmessage:", nats_gps_point)
+    
+    # Check if this is a control message signifying the end of the transmission
+    if nats_gps_point == "END": 
+      print('End control message received. No more gps points to check')
+      nonlocal fin
+      fin.set_result("END")
+      return
+
+    # Check for NAT's point
+    print('Verifying that nats', nats_gps_point['time'], ' is in the database')
+    rst = db.one('SELECT * FROM gps where time = %s;', (nats_gps_point['time'],) )
+    if rst is not None and len(rst) == 3 and rst.lat == nats_gps_point['lat'] and rst.lng == nats_gps_point['lon']:
+      print('nats timestamp', nats_gps_point['time'], 'successfully entered into the database')
+    else:
+      print('nats GPS point', nats_gps_point, '\nwas not successfully entered into database!\ndb entry:', rst)
+      sys.exit(-1)
+      
+    # Counter just for fun
+    global points_received
+    points_received = points_received + 1
+    sys.stdout.flush()
+    return
+
+
+  print("setting up gps message callback")
+  sid = await nc.subscribe("gps", cb=message_handler)
+  print("gps message callback setup")
   sys.stdout.flush()
+  
+  await nc.flush(1)
 
-print('Successfully verified %d points' % points_received)
-sys.exit(0)
+  await fin
+  print("End message received. Exiting")
+  await nc.close()
+
+if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(run(loop))
+    loop.close()
+    
+    print('Successfully verified %d points' % points_received)
+    sys.exit(0)
 
