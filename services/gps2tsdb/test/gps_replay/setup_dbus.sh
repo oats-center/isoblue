ffcc26261fe19647486ddf31a3879c342ac978eb
Aaron Neustedter <aneusted@purdue.edu>
2021-02-06 13:44:09 -0500

    gps2tsdb Testing (#109)
    Introduction of two testing containers to test the gps2tsdb container. `gps_replay` will be useful for testing other containers that read real time GPS data from `gpsd`

---
+++ services/gps2tsdb/test/gps_replay/setup_dbus.sh
@@ -0,0 +1,18 @@
+# Setup dbus
+# Taken from here: https://georgik.rocks/how-to-start-d-bus-in-docker-container/
+
+# Remove anything from the previous run
+echo "Cleaning up from previous runs"
+rm -fr /var/run/dbus
+
+# Generate missing machine-id by command
+echo "Generating missing machine-ids"
+dbus-uuidgen > /var/lib/dbus/machine-id
+
+# Start the D-Bus daemon inside the container
+echo "Starting dbus daemon"
+mkdir -p /var/run/dbus
+dbus-daemon --config-file=/usr/share/dbus-1/system.conf --print-address
+
+exec python ./gps_replay.py
+
