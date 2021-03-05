119dea068892935b39593c7c4eab3d3e194de9f9
Andrew Balmos <abalmos@purdue.edu>
2021-03-05 17:56:15 -0500

    Working now. On dev05 in testing.

--- services/gps-exporter/entrypoint.sh
+++ services/gps-exporter/entrypoint.sh
@@ -11,7 +11,7 @@ declare -p | grep -Ev 'BASHOPTS|BASH_VERSINFO|EUID|PPID|SHELLOPTS|UID' > /app/co
 # redirecting to proc/1/fd/1 prints to the docker log
 echo "SHELL=/bin/bash
 BASH_ENV=/app/container.env
-*/1 * * * * /app/run-gps-container.sh > /proc/1/fd/1 2>&1
+*/1 * * * * /app/run-gps-exporter.sh > /proc/1/fd/1 2>&1
 # This extra line makes it a valid cron" > /app/scheduler.txt
 
 crontab /app/scheduler.txt
