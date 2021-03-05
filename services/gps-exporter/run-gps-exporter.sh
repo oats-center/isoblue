fd651f0d0d0731c13d0c25f2d57515010e6e5209
Andrew Balmos <abalmos@purdue.edu>
2021-03-05 17:43:09 -0500

    Try redirecting everywhere

--- services/gps-exporter/run-gps-exporter.sh
+++ services/gps-exporter/run-gps-exporter.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-echo "\$db_user = $db_user"
-echo "\$db_host = $db_host"
-echo "\$db_database = $db_database"
-echo "\$from = $from"
-echo "\$segment_length = $segment_length"
+echo "\$db_user = $db_user" > /proc/1/fd/1 2>&1
+echo "\$db_host = $db_host" > /proc/1/fd/1 2>&1
+echo "\$db_database = $db_database" > /proc/1/fd/1 2>&1
+echo "\$from = $from" > /proc/1/fd/1 2>&1
+echo "\$segment_length = $segment_length" > /proc/1/fd/1 2>&1
 
-/app/gps-exporter postgress://$db_user:$db_password@$db_host:$db_port/$db_database /data -f "$from" -l $segment_length --append
+/app/gps-exporter postgress://$db_user:$db_password@$db_host:$db_port/$db_database /data -f "$from" -l $segment_length --append > /proc/1/fd/1 2>&1
