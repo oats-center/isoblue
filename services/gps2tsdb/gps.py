8fc31620e4d9eb06abb70abd2e0ed34c9d2d0b75
Aaron Neustedter <aneusted@purdue.edu>
2021-04-22 18:40:07 -0400

    new database, added inserts

--- services/gps2tsdb/gps.py
+++ services/gps2tsdb/gps.py
@@ -12,14 +12,76 @@ from nats.aio.errors import ErrConnectionClosed, ErrTimeout, ErrNoServers
 def setup_db_tables(db):
     print("Ensuring timescaledb extension is activated")
     db.run("CREATE EXTENSION IF NOT EXISTS timescaledb;")
+
+    # Create GPS tpv table
     print("Ensuring tables are setup properly")
+    # Ref Table 1 from here: https://gpsd.gitlab.io/gpsd/gpsd_json.html
+    # Most of these columns will not be used
+    # Sample TPV message from out device:
+    # { "class":  "TPV",
+    #   "device": "/dev/ttyUSB0",
+    #   "mode":    3,
+    #   "time":   "2021-04-22T20:53:23.000Z",
+    #   "ept":     0.005,
+    #   "lat":     40.421641854,
+    #   "lon":    -86.916256391,
+    #   "alt":     200.172,
+    #   "epx":     31.500,
+    #   "epy":     23.863,
+    #   "epv":     106.490,
+    #   "track":   359.0756,
+    #   "speed":   0.590,
+    #   "climb":   0.002,
+    #   "eps":     1.66,
+    #   "epc":     212.98 }
     db.run("""
-            CREATE TABLE IF NOT EXISTS gps (
+            CREATE TABLE IF NOT EXISTS gps.tpv (
+              device text
+              mode smallint NOT NULL
+              status smallint
               time timestamptz UNIQUE NOT NULL,
-              lat double precision NOT NULL,
-              lng double precision NOT NULL);""")
+              altHAE double precision
+              altMSL double precision
+              alt double precision
+              climb double precision
+              datum text
+              depth double precision
+              dgpsAge double precision
+              dgpsSta dobule precision
+              epc double precision
+              epd double precision
+              eph double precision
+              eps double precision
+              ept double precision
+              epx double precision
+              epy double precision
+              epv double precision
+              geoidSep double precision
+              lat double precision
+              lon double precision
+              track double precision
+              magtrack double precision
+              magvar double precision
+              speed double precision
+              ecefx double precision
+              ecefy double precision
+              ecefz double precision
+              ecefpACC double precision
+              ecefvx double precision
+              ecefvy double precision
+              ecefvz double precision
+              ecefvACC double precision
+              sep double precision
+              relD double precision
+              relE double precision
+              relN double precision
+              wanglem double precision
+              wangler double precision
+              wanglet double precision
+              wspeedr double precision
+              wspeedt double precision);""")
     print("Ensuring GPS point table is a timescaledb hypertable")
-    db.run("SELECT create_hypertable('gps', 'time', if_not_exists => TRUE, migrate_data => TRUE);")
+    db.run("SELECT create_hypertable('gps.tpv', 'time', if_not_exists => TRUE, migrate_data => TRUE);")
     print("Finished settting up tables")
 
 
@@ -66,33 +128,34 @@ async def run(loop):
     print("Connecting to nats server")
     await nc.connect("nats://nats:4222")
 
+    print("Connecting to database")
+    db = connect_db()
 
+    print("Setting up db")
+    setup_db_tables(db) 
 
-    async def notify_sms(msg):
+    async def notify_tpv(msg):
 
         subject = msg.subject
         reply = msg.reply
         data = msg.data.decode()
+        tpv_fix  = json.loads(data)
         print("Received a message on '{subject} {reply}': {data}".format(
             subject=subject, reply=reply, data=data))
 
+        # build sql query from json
+        if "time" in tpv_fix:
+            print("Inserting for timestamp", gps_tpv["time"])
+            db.run("INSERT INTO gps.tpv select (json_populate_record(null:gps.tpv,
+                   '%s'::json)).*;", data)
+        else:
+          print("GPS point had no timestamp, could not insert into time-series database")
         sys.stdout.flush()
 
 
-    async def new_subscriber(msg):
-        subject = msg.subject
-        reply = msg.reply
-        data = msg.data.decode()
-        print("Received a message on '{subject} {reply}': {data}".format(
-            subject=subject, reply=reply, data=data))
-
-    notify_subject = 'sms' 
+    notify_subject = 'gps.tpv' 
     print("Subscribing to subject", notify_subject)
-    await nc.subscribe(notify_subject, cb=notify_sms)
-
-    subscribe_subject = "isoblue.notifications.sms.new_subscriber"
-    print("Subscribing to subject", subscribe_subject)
-    await nc.subscribe(subscribe_subject, cb=new_subscriber)
+    await nc.subscribe(notify_subject, cb=notify_tpv)
 
     while True:
       await asyncio.sleep(1)
