064142bbc49d83683ba22d55a149f16db321c3c4
Fabio A. Castiblanco <fcastibl@purdue.edu>
2023-08-11 15:33:01 -0400

    added GPSD class filtering and performed code cleanup

--- services/gps_nats/gps.py
+++ services/gps_nats/gps.py
@@ -3,7 +3,6 @@ import os
 import asyncio
 import nats
 import json
-import ast
 from gpsdclient import GPSDClient
 
 print("Starting gps_nats...")
@@ -12,36 +11,39 @@ async def main():
 
 
     # Create nats object and connect to local nats server
-    print('Connecting to NATS server')
-    nc = await nats.connect("nats://localhost:4222")
-    # Get list of GPSD classes to send over NATS
-    gpsd_classes = "[" + os.getenv('GPSD_CLASSES') + "]"
-    # Use AST to parse the classes string into a list
+    print('Connecting to NATS server...')
     try:
-        gpsd_classes = ast.literal_eval(gpsd_classes)
+        nc = await nats.connect("nats://localhost:4222")
+        print("NATS connection successful!")
     except:
-        print("Failed to parse GPSD_CLASSES env. variable. Defaulting to all classes")
-        gpsd_classes=None
-
+        print("Failed to connect to NATS server")
+    # Get list of GPSD classes to send over NATS
+    gpsd_classes = os.getenv('GPSD_CLASSES').split(',')
+    
+    # Read from GPSD socket
     with GPSDClient(host="127.0.0.1") as client:
-        for result in client.dict_stream(filter=gpsd_classes)
-        # Parse incoming GPSD JSON strings
-        try:
-            fix = json.loads(results)
-        except:
+        for result in client.json_stream(filter=gpsd_classes):
+            # Parse incoming GPSD JSON strings
+            try:
+                msg = json.loads(result)
+            except:
             # Ignore string if unable to parse
-            print("Failed to parse JSON message")
-        subject = os.getenv('AVENA_PREFIX') + ".gps." + str(fix["class"])
-        #print("Publishing new data point to subject", subject, ": ", new_data[:-1])
-        await nc.publish(subject, bytes(new_data, 'utf-8'))
-    # Loop runs out of control without this
-    #sleep(0.001)
+                print("Failed to parse JSON message")
+                continue
+            subject = f"{os.getenv('AVENA_PREFIX')}.gps.{msg['class']}"
+            # Publish received JSON message
+            await nc.publish(subject, bytes(result, 'utf-8'))
+            await asyncio.sleep(0.001)
 
 if __name__ == '__main__':
 
+    loop = asyncio.get_event_loop()
+    asyncio.ensure_future(main())
+        
     try:
-        asyncio.run(main())
-    finally:
+        loop.run_forever()
+    except(KeyboardInterrupt):
         print("Stopping gps_nats...")
+        quit()
+    loop.close()
 
-    sys.exit(0)
